6725af0767bade5a6ecf9978088a8e661018ad98
likhitha <likitha.pulluru@cdk.com>
2017-07-17 20:39:09 +0530

    Basic Project Structure created

---
+++ MyApp/MyApp_API/Controllers/VehicleInfoesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MyApp_API.Models;
+
+namespace MyApp_API.Controllers
+{
+    public class VehicleInfoesController : ApiController
+    {
+        private ProjectDBEntities1 db = new ProjectDBEntities1();
+
+        // GET: api/VehicleInfoes
+        public IQueryable<VehicleInfo> GetVehicleInfoes()
+        {
+            return db.VehicleInfoes;
+        }
+
+        // GET: api/VehicleInfoes/5
+        [ResponseType(typeof(VehicleInfo))]
+        public IHttpActionResult GetVehicleInfo(int id)
+        {
+            VehicleInfo vehicleInfo = db.VehicleInfoes.Find(id);
+            if (vehicleInfo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(vehicleInfo);
+        }
+
+        // PUT: api/VehicleInfoes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutVehicleInfo(int id, VehicleInfo vehicleInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != vehicleInfo.vid)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(vehicleInfo).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VehicleInfoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/VehicleInfoes
+        [ResponseType(typeof(VehicleInfo))]
+        public IHttpActionResult PostVehicleInfo(VehicleInfo vehicleInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.VehicleInfoes.Add(vehicleInfo);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = vehicleInfo.vid }, vehicleInfo);
+        }
+
+        // DELETE: api/VehicleInfoes/5
+        [ResponseType(typeof(VehicleInfo))]
+        public IHttpActionResult DeleteVehicleInfo(int id)
+        {
+            VehicleInfo vehicleInfo = db.VehicleInfoes.Find(id);
+            if (vehicleInfo == null)
+            {
+                return NotFound();
+            }
+
+            db.VehicleInfoes.Remove(vehicleInfo);
+            db.SaveChanges();
+
+            return Ok(vehicleInfo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool VehicleInfoExists(int id)
+        {
+            return db.VehicleInfoes.Count(e => e.vid == id) > 0;
+        }
+    }
+}
